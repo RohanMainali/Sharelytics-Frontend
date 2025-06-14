f8ce0690f7d9a9043050ab8871cb97787e3e137e
Rohan Mainali <rohanmainali@gmail.com>
2025-06-14 20:49:18 +0545

    update

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,14 @@
 "use client";
 import dynamic from "next/dynamic";
+import { Suspense } from "react";
 
 // Dynamically import LoginForm component with SSR disabled
 const LoginForm = dynamic(() => import("./LoginForm") as Promise<{ default: React.ComponentType<any> }>, { ssr: false });
 
 export default function LoginPage() {
-  return <LoginForm />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <LoginForm />
+    </Suspense>
+  );
 }
